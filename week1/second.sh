ea00c51ceaff9e11d8108ff356a91a3e15faec2e
danieldacey <daniel.dacey65@gmail.com>
2021-07-19 02:03:23 +0000

    Updated header and comments

--- week1/second.sh
+++ week1/second.sh
@@ -1,4 +1,15 @@
-#!/bin/bash
+#!/bin/bash -   
+#title          :second.sh
+#description    :Using a command line argument
+#author         :Daniel Dacey
+#date           :20210719
+#version        :1.0      
+#usage          :./second.sh
+#notes          :     
+#bash_version   :5.0.17(1)-release
+#============================================================================
+
 echo "Hi There!"
+#$1 Takes the command line argument and prints it as part of the echo command
 echo "It's good to see you $1!"
 exit 0
