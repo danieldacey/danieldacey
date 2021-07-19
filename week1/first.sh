ef5553b7acce21637f437b5814965df042be7792
danieldacey <daniel.dacey65@gmail.com>
2021-07-19 01:42:18 +0000

    Update file header and comments

--- week1/first.sh
+++ week1/first.sh
@@ -1,3 +1,14 @@
-#!/bin/bash
+#!/bin/bash -   
+#title          :first.sh
+#description    :Example of echo comment to print to the terminal
+#author         :Daniel Dacey
+#date           :20210719
+#version        :1      
+#usage          :./first.sh
+#notes          :       
+#bash_version   :5.0.17(1)-release
+#============================================================================
+
 echo "Hi There!"
-exit 22
+#return code
+exit 0
