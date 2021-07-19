197a71bdb7901fda7053276eb1799097557ba910
danieldacey <daniel.dacey65@gmail.com>
2021-07-19 03:27:14 +0000

    Updated comments

--- week2/setPassword.sh
+++ week2/setPassword.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -   
 #title          :setPassword.sh
-#description    :Example of echo command to print to the terminal
+#description    :Save a password to a text file in a directory nominated by the user
 #author         :Daniel Dacey
 #date           :20210719
 #version        :1      
@@ -10,10 +10,11 @@
 #============================================================================
 
 #Get the folder name to save
-#Note there is no error checking so we presume the folder name is valid
+#There is no error checking so we presume the folder name is valid
 read -p 'Please enter a folder name: ' folder_var
-#Get the users password from the terminal and save 
-#in a file called 'secret.txt' which is inside the folder chosen by the user
+#Get the users password from the terminal while hiding the user input 
 read -sp "Please enter a secret password: " pword_var
+#Use a pipe to save the pword variable in a file called 'secret.txt' 
+#which is inside the folder chosen by the user in the variable folder_var
 echo "$pword_var" > $folder_var/secret.txt
 exit 0
