0c0c48f2162d0403a0c2c1c6b2ca5dc959eed66c
danieldacey <daniel.dacey65@gmail.com>
2021-08-01 22:48:00 +0000

    Add colour text

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -3,23 +3,30 @@
 #description    :Check password hash in file against user input
 #author         :Daniel Dacey
 #date           :20210725
-#version        :1      
+#version        :1.1      
 #usage          :./passwordCheck.sh
 #notes          :Example of conditional decision making       
 #bash_version   :5.0.17(1)-release
 #============================================================================
 
+#Variables to hold colors
+Red="`tput setaf 1`"
+Green="`tput setaf 2`"
+White="`tput setaf 7`"
+
 #Ask the user to type a secret password (hide that input)
-read -sp "Please enter a secret password: " pword_var
+read -sp $"${Red} Enter Password: " pword_var
 #Check the user’s password against the hash stored in 'secret.txt' using sh256sum for a match
 echo $pword_var | sha256sum secret.txt --check --quiet --status
 #Use the exit code from sha256sum using '$?'  
 #and advise the user if they have access granted and exit on code 0 with match
 #otherwise print access denied with exit code 1
 if [ $? -eq 0 ]; then 
-     echo -e "\nAccess Granted"
+     echo -e "\n${Green}Access Granted"
+     echo -e "${White}"
      exit 0
 else
-     echo -e "\nAccess Denied"
+     echo -e "\n${Red}Access Denied"
+     echo -e "${White}"
      exit 1
 fi
