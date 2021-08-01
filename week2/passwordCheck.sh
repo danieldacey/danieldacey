74130483acd5e0cd9213ae3c5951d0109856893c
danieldacey <daniel.dacey65@gmail.com>
2021-08-01 22:59:49 +0000

    Set correct filepath for bash file

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -3,7 +3,7 @@
 #description    :Check password hash in file against user input
 #author         :Daniel Dacey
 #date           :20210725
-#version        :1.1      
+#version        :1.2     
 #usage          :./passwordCheck.sh
 #notes          :Example of conditional decision making       
 #bash_version   :5.0.17(1)-release
@@ -15,9 +15,9 @@ Green="`tput setaf 2`"
 White="`tput setaf 7`"
 
 #Ask the user to type a secret password (hide that input)
-read -sp $"${Red} Enter Password: " pword_var
+read -sp $"${Red}Enter Password: " pword_var
 #Check the user’s password against the hash stored in 'secret.txt' using sh256sum for a match
-echo $pword_var | sha256sum secret.txt --check --quiet --status
+echo $pword_var | sha256sum ~/scripts/portfolio/week2/secret.txt --check --quiet --status
 #Use the exit code from sha256sum using '$?'  
 #and advise the user if they have access granted and exit on code 0 with match
 #otherwise print access denied with exit code 1
