841de7f0a80af01f359719dc495d906ed293a4b5
danieldacey <daniel.dacey65@gmail.com>
2021-08-02 01:43:03 +0000

    Add version and comments

--- week2/foldercopier.sh
+++ week2/foldercopier.sh
@@ -1,4 +1,13 @@
-#!/bin/bash      
+#!/bin/bash
+#title          :foldercopier.sh
+#description    :Copy folder to new destination
+#author         :Daniel Dacey
+#date           :20210725
+#version        :1
+#usage          :./foldercopier.sh
+#notes          :
+#bash_version   :5.0.17(1)-release
+#============================================================================     
 read -p "type the name of the folder you would like to copy: " folderName 
       
 #if the name is a valid directory       
