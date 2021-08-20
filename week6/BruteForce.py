52b6bb922bfd6af2969a435e0ebd9d8ec886b12f
danieldacey <daniel.dacey65@gmail.com>
2021-08-20 08:12:10 +0000

    Working build
    Fixed password length
    Cleaned code
    Commented code

--- week6/BruteForce.py
+++ week6/BruteForce.py
@@ -1,60 +1,50 @@
 #!/usr/bin/python3
-
-
+#Based on code found at
 #http://www.instructables.com/id/Password-Brute-forcer-in-Python/
+#Author Daniel Dacey
+#Version 1
+#20210820
+
 import hashlib
 import itertools
 import time
 
-
-#hidden password hash
-#passwordHash = "2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824"
-
+#Search criteria to match on is numbers and letters
 Alphabet = ("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890-_.")
-CharLength = 1
 
 #Get Password to crack
 Password = input("What is the password: ")
 
-#Use the time library to display the time to crack the password
+#Use the time library to track the time to crack the password
 start = time.time()
 
-counter = 1  
-CharLength = 1
-print(counter, CharLength)
+counter = 1  #Keep track of number of attempts with counter
+CharLength = len(Password)  #Length of password
 
-for CharLength in range(25):
- passwords = (itertools.product(Alphabet, repeat = CharLength))
- print("\n \n")
-
- print("currently working on passwords with ", CharLength, " chars")
- print("We are currently at ", (counter / (time.time() - start)), "attempts per seconds")
- print("It has been ", time.time() - start, " seconds!")
- print("We have tried ", counter, " possible passwords!")
- for i in passwords:
-    counter += 1
-    i = str(i)
-    i = i.replace("[", "")
-    i = i.replace("]", "")
-    i = i.replace("'", "")
-    i = i.replace(" ", "")
-    i = i.replace(",", "")
-    i = i.replace("(", "")
-    i = i.replace(")", "")
-    if i == Password:
-        end = time.time()
-        timetaken = end - start
- 
-print("Found it in ", timetaken, " seconds and ", counter, "attempts")
-print("That is ", counter / timetaken, " attempts per second!")
-print(i)
-input("Press enter when you have finished")
-exit()
-
-
-
-# if(wordlistHash == passwordHash):
-
-#             print(f"Password has been cracked! It was {word}")
-
-#             break
+#Lets check for password match.
+#Password can be a maximum of eight characters
+for CharLength in range(7):
+    passwords = (itertools.product(Alphabet, repeat = CharLength))
+    print("currently working on passwords with ", CharLength, " chars")
+    print("We are currently at ", (counter / (time.time() - start)), "attempts per seconds")
+    print("It has been ", time.time() - start, " seconds!")
+    print("We have tried ", counter, " possible passwords!")
+    for i in passwords:
+        counter += 1
+        i = str(i)
+        i = i.replace("[", "")
+        i = i.replace("]", "")
+        i = i.replace("'", "")
+        i = i.replace(" ", "")
+        i = i.replace(",", "")
+        i = i.replace("(", "")
+        i = i.replace(")", "")
+        if i == Password:
+            #We have a match! Print password and stats
+            end = time.time()
+            timetaken = end - start
+            print("Password has been cracked! It was: ",i)
+            print("Found it in ", timetaken, " seconds and ", counter, "attempts")
+            print("That is ", counter / timetaken, " attempts per second!")            
+            input("Press enter when you have finished")
+            exit()
