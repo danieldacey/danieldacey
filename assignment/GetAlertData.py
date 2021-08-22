35cea816e603afd3c06f2eb84e1c29a08857dd58
danieldacey <daniel.dacey65@gmail.com>
2021-08-22 14:15:33 +0000

    Initial commit

---
+++ assignment/GetAlertData.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+from typing import Text
+from bs4 import BeautifulSoup
+from urllib.request import urlopen
+import re
+from bs4.element import ResultSet
+import sys
+
+#Get Alert Data
+#Filename: GetAlertData.py
+#Author: Daniel Dacey
+#Version: 1.0
+#Description: Web scrapes alerts from cyber.gov.au and returns the results
+
+#Import Libraries
+print(sys.argv[1])
+
+#Get page 
+page = urlopen(sys.argv[1])
+#soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page, 'lxml')
+
+#Get all the 'p' tags in the page 
+names = soup.body.find_all('p')
+
+#Extract the data from the p tags we need using regular expresions in Python
+function_dates = re.findall('[0-9]{2}\s[A-Z][a-z]{2}\s[0-9]{4}',str(names))
+function_alert = re.findall('>[A-Z][A-Z]+<',str(names))
+function_alert = [item[1:len(item)-1] for item in function_alert]
+function_title = re.findall(r'acsc-title">(.*?)</p>',str(names))
+function_summary = re.findall(r'acsc-summary">(.*?)</p>',str(names))
+
+#Return the results
+f = open("data.txt","w")
+for x in range(len(function_title)):
+   f.write(function_dates[x]+":"+function_alert[x]+":"+function_title[x]+":"+function_summary[x]+"\n")
+f.close()
