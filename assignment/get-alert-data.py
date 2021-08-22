36ec4cb4c47f1362b0ecd98b9916df6a91a2b8d6
danieldacey <daniel.dacey65@gmail.com>
2021-08-22 18:39:41 +0000

    Using parameter for page load
    Some code comments updated

--- assignment/get-alert-data.py
+++ assignment/get-alert-data.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+#Import Libraries
 from typing import Text
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
@@ -12,11 +13,10 @@ import sys
 #Version: 1.0
 #Description: Web scrapes alerts from cyber.gov.au and returns the results
 
-#Import Libraries
-print(sys.argv[1])
-
-#Get page 
+#Get page to load from main script
 page = urlopen(sys.argv[1])
+
+#Load page with library BeutifulSoup
 #soup = BeautifulSoup(page, 'html.parser')
 soup = BeautifulSoup(page, 'lxml')
 
@@ -30,7 +30,7 @@ function_alert = [item[1:len(item)-1] for item in function_alert]
 function_title = re.findall(r'acsc-title">(.*?)</p>',str(names))
 function_summary = re.findall(r'acsc-summary">(.*?)</p>',str(names))
 
-#Return the results
+#Return the results to data.txt
 f = open("data.txt","w")
 for x in range(len(function_title)):
    f.write(function_dates[x]+":"+function_alert[x]+":"+function_title[x]+":"+function_summary[x]+"\n")
