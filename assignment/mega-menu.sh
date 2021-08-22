c11c093a5e1011f497bf1fc6fdf8802f34f44d57
danieldacey <daniel.dacey65@gmail.com>
2021-08-22 18:35:42 +0000

    Supports different filters
    Add options to filter alerts by type of business.
    
    Some updates on comments

--- assignment/mega-menu.sh
+++ assignment/mega-menu.sh
@@ -5,7 +5,7 @@
 #date           :20210822
 #version        :2.0      
 #usage          :./mega-menu.sh
-#notes          :      
+#notes          :Adapted from week 3. Now returns an exit code for main script to act on     
 #bash_version   :5.0.17(1)-release
 #============================================================================
 
@@ -16,19 +16,24 @@ Blue='\033[0;34m'
 Cyan='\033[0;36m'
 White='\033[0;37m'
 
-#Menu function
+#Banner
 clear
 figlet "Cyber Alerts"
+echo -en "\n"
+
+#Menu function
 menu_selection () {
  echo -e "${Blue}Please select a report option:"
- echo -e "${Cyan}1. Sort by most recent"
- echo -e "2. Sort by Alert level"
+ echo -e "${Cyan}1. Individuals and mamilies"
+ echo -e "2. Small and medium businesses"
+ echo -e "3. Large organisations"
+ echo -e "4. Government"
  echo -e "${White}0. Exit" 
  read choice
 }
 
 #Check Password
-bash ./passwordCheck.sh
+#bash ./password-check.sh
 #Continue if exit 0 from previous script
 if [ $? -eq 0 ]; then
     #Get menu slection
@@ -36,15 +41,19 @@ if [ $? -eq 0 ]; then
     #validate choice    
     #Continue until user exits
     while [ $choice != 0 ]; do   
-        #Determine script to run based on user input from read
+        #Determine script to run based on user input from read and use exit code to advise parent script of choice
         case $choice in 
-            1)   
-                bash ~/scripts/portfolio/week2/foldermaker.sh ;;
+            1)  
+                exit 1;;                
             2)    
-                bash ~/scripts/portfolio/week2/foldercopier.sh;;                       
+                exit 2;;
+            3)    
+                exit 3;;
+            4)    
+                exit 4;;
         esac
         #Get next menu selection
         menu_selection
     done
+    exit 0
 fi
-echo "Goodbye!"
