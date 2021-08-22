275a9bf693f53352df159b397785ee0272c1e1da
danieldacey <daniel.dacey65@gmail.com>
2021-08-22 16:41:14 +0000

    Table output working
    Includes summary

--- assignment/output-table.sh
+++ assignment/output-table.sh
@@ -8,22 +8,36 @@
 #notes          :
 #bash_version   :5.0.17(1)-release
 #===========================================================================================
+
+#Clear screen and build banner
+clear
+figlet "Cyber Alerts"
+
+#Build Table and display
 awk 'BEGIN {
     #File Seperator is colon
     FS=":";
 
     #Heading
-    print "|\033[34m Username         \033[0m|\033[34m UserID \033[0m|\033[34m GroupID \033[0m|\033[34m Home                            \033[0m|\033[34m Shell             \033[0m|";
+    print "|\033[34m Date    \033[0m    |\033[34m Urgency   \033[0m|\033[34m Title                                                                                      \033[0m|";
 
-    print "|__________________|________|_________|_________________________________|___________________|";
+    print "|_____________|___________|____________________________________________________________________________________________|";
 }
 
-#Find only /bin/bash shell users
-/bin[/]bash/ {
+#Read Cyber Alerts and display
+ {
     #Detail line with colors and spacing      
-    printf("| \033[33m%-16s\033[0m | \033[35m%-6s\033[0m | \033[35m%-7s\033[0m | \033[35m%-31s\033[0m | \033[35m%-18s\033[0m| \n", $1, $3, $4, $6, $7);
+    printf("| \033[33m%-11s\033[0m | \033[31m%-8s\033[0m  | \033[37m%-90s\033[0m |\n", $1, $2, $3);
 }
 
 END {   
+    print "|_____________|___________|____________________________________________________________________________________________|";
+}' data.txt
 
-}' /etc/passwd
+#Collect some stats and display
+linecount=$(wc -l data.txt | awk '{ print $1 }')
+criticalcount=$(grep -o 'CRITICAL' data.txt | wc -l)
+reportdate=$(date +"%d-%m-%Y")
+echo "$(tput setaf 6)Total number of alerts: $linecount"
+echo "$(tput setaf 1)Critical alerts: $criticalcount"
+echo "$(tput setaf 6)Report generated: $reportdate"
